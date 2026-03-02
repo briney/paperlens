181e81782ab67d7ca8715525119f7e006fdfd281
Bryan Briney <bryanbriney@gmail.com>
2026-03-01 22:10:00 -0800

    Fix ANSI escape codes in setup.sh and Docker networking on macOS
    Use $'...' ANSI-C quoting for color variables so escape codes work
    in read -p prompts (not just echo -e). Replace network_mode: host
    with explicit port mappings since host networking doesn't work on
    macOS Docker Desktop.

--- setup.sh
+++ setup.sh
@@ -26,14 +26,14 @@ fi
 # Color / formatting helpers (respects NO_COLOR and non-TTY)
 # ---------------------------------------------------------------------------
 if [[ -z "${NO_COLOR:-}" ]] && [[ -t 1 ]]; then
-    RED='\033[0;31m'
-    GREEN='\033[0;32m'
-    YELLOW='\033[0;33m'
-    BLUE='\033[0;34m'
-    CYAN='\033[0;36m'
-    BOLD='\033[1m'
-    DIM='\033[2m'
-    RESET='\033[0m'
+    RED=$'\033[0;31m'
+    GREEN=$'\033[0;32m'
+    YELLOW=$'\033[0;33m'
+    BLUE=$'\033[0;34m'
+    CYAN=$'\033[0;36m'
+    BOLD=$'\033[1m'
+    DIM=$'\033[2m'
+    RESET=$'\033[0m'
 else
     RED='' GREEN='' YELLOW='' BLUE='' CYAN='' BOLD='' DIM='' RESET=''
 fi
